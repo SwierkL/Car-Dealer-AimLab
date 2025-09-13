d66d9b6e5675ad7cb88205a47a69ecb697d04bd8
lswierk <lswierk3@wp.pl>
2025-09-13 20:54:55 +0200

    Przyspieszenie 0.35

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function activateRandomCell() {
 function startGameLoop() {
   setInterval(() => {
     activateRandomCell();
-  }, 300); //Tempo pojawiania się
+  }, 350); //Tempo pojawiania się
 }
 
 function showFailure() {
